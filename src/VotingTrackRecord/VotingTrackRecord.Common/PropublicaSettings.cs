7096fc1c049bd01365eabfb965941622e39e6c04
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-05-15 11:02:34 -0700

    Add propublica settings singleton

--- src/VotingTrackRecord/VotingTrackRecord.Common/PropublicaSettings.cs
+++ src/VotingTrackRecord/VotingTrackRecord.Common/PropublicaSettings.cs
@@ -2,6 +2,6 @@
 {
     public class PropublicaSettings
     {
-        public string ApiKey { get; set; }
+        public string? ApiKey { get; set; }
     }
 }
--- src/VotingTrackRecord/VotingTrackRecord/Program.cs
+++ src/VotingTrackRecord/VotingTrackRecord/Program.cs
@@ -11,16 +11,14 @@ namespace VotingTrackRecord
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var propublicaSettings = builder.Configuration.GetSection("PropublicaSettings").Get<PropublicaSettings>();
+            builder.Services.AddSingleton(propublicaSettings);
 
             var app = builder.Build();
-
             
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
