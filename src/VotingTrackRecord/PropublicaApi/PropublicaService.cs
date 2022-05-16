1cc8d6c0457bcac13d7d6828b7d3b3fac127de01
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-05-16 14:27:32 -0700

    Add properties for json covert

--- src/VotingTrackRecord/PropublicaApi/PropublicaService.cs
+++ src/VotingTrackRecord/PropublicaApi/PropublicaService.cs
@@ -22,10 +22,18 @@ namespace Propublica
         public async Task<string> GetRecentVotesAsync(string chamber)
         {
             var apiService = RestService.For<IPropublicaApi>(propublicaSettings.Url);
-            
-            var result = await apiService.GetRecentVotesAsync(chamber, propublicaSettings.ApiKey);
+            try
+            {
+                var result = await apiService.GetRecentVotesAsync(chamber, propublicaSettings.ApiKey);
 
-            return null;
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
         }
     }
 
@@ -34,19 +42,7 @@ namespace Propublica
         [Get("/{chamber}/votes/recent.json")]
         Task<Root> GetRecentVotesAsync(string chamber, [Header("X-API-KEY")] string apiKey);
     }
-
     // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
-    public class Root
-    {
-        [JsonPropertyName("status")]
-        public string Status;
-
-        [JsonPropertyName("copyright")]
-        public string Copyright;
-              
-        public Results Results;
-    }
-
     public class Amendment
     {
     }
@@ -54,167 +50,179 @@ namespace Propublica
     public class Bill
     {
         [JsonPropertyName("bill_id")]
-        public string BillId;
+        public string BillId { get; set; }
 
         [JsonPropertyName("number")]
-        public string Number;
+        public string Number { get; set; }
 
         [JsonPropertyName("sponsor_id")]
-        public string SponsorId;
+        public string SponsorId { get; set; }
 
         [JsonPropertyName("api_uri")]
-        public string ApiUri;
+        public string ApiUri { get; set; }
 
         [JsonPropertyName("title")]
-        public string Title;
+        public string Title { get; set; }
 
         [JsonPropertyName("latest_action")]
-        public string LatestAction;
+        public string LatestAction { get; set; }
     }
 
     public class Democratic
     {
         [JsonPropertyName("yes")]
-        public int Yes;
+        public int Yes { get; set; }
 
         [JsonPropertyName("no")]
-        public int No;
+        public int No { get; set; }
 
         [JsonPropertyName("present")]
-        public int Present;
+        public int Present { get; set; }
 
         [JsonPropertyName("not_voting")]
-        public int NotVoting;
+        public int NotVoting { get; set; }
 
         [JsonPropertyName("majority_position")]
-        public string MajorityPosition;
+        public string MajorityPosition { get; set; }
     }
 
     public class Independent
     {
         [JsonPropertyName("yes")]
-        public int Yes;
+        public int Yes { get; set; }
 
         [JsonPropertyName("no")]
-        public int No;
+        public int No { get; set; }
 
         [JsonPropertyName("present")]
-        public int Present;
+        public int Present { get; set; }
 
         [JsonPropertyName("not_voting")]
-        public int NotVoting;
+        public int NotVoting { get; set; }
     }
 
     public class Republican
     {
         [JsonPropertyName("yes")]
-        public int Yes;
+        public int Yes { get; set; }
 
         [JsonPropertyName("no")]
-        public int No;
+        public int No { get; set; }
 
         [JsonPropertyName("present")]
-        public int Present;
+        public int Present { get; set; }
 
         [JsonPropertyName("not_voting")]
-        public int NotVoting;
+        public int NotVoting { get; set; }
 
         [JsonPropertyName("majority_position")]
-        public string MajorityPosition;
+        public string MajorityPosition { get; set; }
     }
 
     public class Results
     {
         [JsonPropertyName("chamber")]
-        public string Chamber;
+        public string Chamber { get; set; }
 
         [JsonPropertyName("offset")]
-        public int Offset;
+        public int Offset { get; set; }
 
         [JsonPropertyName("num_results")]
-        public int NumResults;
+        public int NumResults { get; set; }
 
         [JsonPropertyName("votes")]
-        public List<Vote> Votes;
+        public List<Vote> Votes { get; set; }
     }
+
+    public class Root
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonPropertyName("results")]
+        public Results Results { get; set; }
+    }
+
     public class Total
     {
         [JsonPropertyName("yes")]
-        public int Yes;
+        public int Yes { get; set; }
 
         [JsonPropertyName("no")]
-        public int No;
+        public int No { get; set; }
 
         [JsonPropertyName("present")]
-        public int Present;
+        public int Present { get; set; }
 
         [JsonPropertyName("not_voting")]
-        public int NotVoting;
+        public int NotVoting { get; set; }
     }
 
     public class Vote
     {
         [JsonPropertyName("congress")]
-        public int Congress;
+        public int Congress { get; set; }
 
         [JsonPropertyName("chamber")]
-        public string Chamber;
+        public string Chamber { get; set; }
 
         [JsonPropertyName("session")]
-        public int Session;
+        public int Session { get; set; }
 
         [JsonPropertyName("roll_call")]
-        public int RollCall;
+        public int RollCall { get; set; }
 
         [JsonPropertyName("source")]
-        public string Source;
+        public string Source { get; set; }
 
         [JsonPropertyName("url")]
-        public string Url;
+        public string Url { get; set; }
 
         [JsonPropertyName("vote_uri")]
-        public string VoteUri;
+        public string VoteUri { get; set; }
 
         [JsonPropertyName("bill")]
-        public Bill Bill;
+        public Bill Bill { get; set; }
 
         [JsonPropertyName("amendment")]
-        public Amendment Amendment;
+        public Amendment Amendment { get; set; }
 
         [JsonPropertyName("question")]
-        public string Question;
+        public string Question { get; set; }
 
         [JsonPropertyName("question_text")]
-        public string QuestionText;
+        public string QuestionText { get; set; }
 
         [JsonPropertyName("description")]
-        public string Description;
+        public string Description { get; set; }
 
         [JsonPropertyName("vote_type")]
-        public string VoteType;
+        public string VoteType { get; set; }
 
         [JsonPropertyName("date")]
-        public string Date;
+        public string Date { get; set; }
 
         [JsonPropertyName("time")]
-        public string Time;
+        public string Time { get; set; }
 
         [JsonPropertyName("result")]
-        public string Result;
+        public string Result { get; set; }
 
         [JsonPropertyName("democratic")]
-        public Democratic Democratic;
+        public Democratic Democratic { get; set; }
 
         [JsonPropertyName("republican")]
-        public Republican Republican;
+        public Republican Republican { get; set; }
 
         [JsonPropertyName("independent")]
-        public Independent Independent;
+        public Independent Independent { get; set; }
 
         [JsonPropertyName("total")]
-        public Total Total;
+        public Total Total { get; set; }
     }
-
 
 
 }
